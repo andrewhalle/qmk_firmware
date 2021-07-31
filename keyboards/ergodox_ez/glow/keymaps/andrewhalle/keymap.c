4387c59699426bfafbb67545ef24d458d398f40b
Andrew Halle <ahalle@berkeley.edu>
2021-07-30 23:01:29 -0700

    remove layer toggles on semicolon and quote

--- keyboards/ergodox_ez/glow/keymaps/andrewhalle/keymap.c
+++ keyboards/ergodox_ez/glow/keymaps/andrewhalle/keymap.c
@@ -51,14 +51,14 @@ enum custom_keycodes {
   TMUX_SETUP,
 };
 
-const uint16_t PROGMEM alt_pause[] = {KC_A, KC_S, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {COMBO(alt_pause, LALT(KC_PAUSE))};
+// const uint16_t PROGMEM alt_pause[] = {KC_A, KC_S, COMBO_END};
+// combo_t key_combos[COMBO_COUNT] = {COMBO(alt_pause, LALT(KC_PAUSE))};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LBRACKET,                                    KC_RBRACKET,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LCBR,                                        KC_RCBR,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),LGUI_T(KC_QUOTE),
+    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON, KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LPRN,                                        KC_RPRN,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
     WEBUSB_PAIR,    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_LALT,                                                                                                        KC_RALT,        TMUX_SETUP, KC_TRANSPARENT, KC_TRANSPARENT, MO(1),
                                                                                                     KC_TRANSPARENT, KC_PAUSE,       KC_LALT,        LCTL_T(KC_ESCAPE),
